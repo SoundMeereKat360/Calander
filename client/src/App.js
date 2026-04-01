330e8e20bf595e670e1f42a320fa46aca4af6808
Dylan <dylan_sandusky@mail.tmcc.edu>
2026-03-31 22:46:34 -0700

    Fix App useMemo import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 import EventForm from './components/EventForm';
